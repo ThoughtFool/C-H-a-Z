fac6f98e8678d290047b87686bd8bf46eaa79de3
ThoughtFool <thotfool@gmail.com>
2019-12-24 17:25:04 -0500

    adding the ability to heal like-type pawns based on proximity

--- dist/main.js
+++ dist/main.js
@@ -1711,6 +1711,98 @@ module.exports = friendOrFoe = function (homeSquare_ID, adjacentSquaresArr, pawn
                 if (spaceToCheck.childNodes[0].classList.contains(`${pawnType}-pawn`)) {
                 // if (spaceToCheck.childNodes[0].classList.contains("human-pawn")) {
                     console.log(`Hey ${pawnType}, there's strength in numbers!`);
+                    let currentSpace = spaceToCheck.childNodes[0].classList;
+                    if (currentSpace.contains("zombie-pawn")) {
+                        for (let i = 0; i < pawnStats[pawnType].pawnSpawn.length; i++) {
+
+                            homePawnLoc = pawnStats[pawnType].pawnSpawn[i].loc;
+
+                            console.log("homePawnLoc");
+                            console.log(homePawnLoc);
+
+                            console.log("homeSquare_ID");
+                            console.log(homeSquare_ID);
+
+                            if (homePawnLoc == homeSquare_ID) {
+                                console.log("they match!");
+                                let rand = Math.floor(Math.random() * 5);
+                                health = pawnStats[pawnType].pawnSpawn[i].health - rand;
+                                // health = pawnStats[pawnType].pawnSpawn[i].health - 10;
+                                // TODO: need to create a way to increase/decrease health of "allies" by proximity (use adjacentSpaces function):
+                                if (health > -11 && health < 111) {
+
+                                    pawnStats[pawnType].pawnSpawn[i].health = health;
+                                    console.log(spaceToCheck.childNodes[0].id);
+                                    console.log("lastchild");
+                                    console.log(document.getElementById(homeSquare_ID).lastChild);
+                                    healthInfo(pawnType, pawnId);
+                                };
+                            };
+                        };
+                    } else if (currentSpace.contains("cyborg-pawn")) {
+                        for (let i = 0; i < pawnStats[pawnType].pawnSpawn.length; i++) {
+
+                            homePawnLoc = pawnStats[pawnType].pawnSpawn[i].loc;
+
+                            console.log("homePawnLoc");
+                            console.log(homePawnLoc);
+
+                            console.log("homeSquare_ID");
+                            console.log(homeSquare_ID);
+
+                            if (homePawnLoc == homeSquare_ID) {
+                                console.log("they match!");
+                                let rand = Math.floor(Math.random() * 5);  // TODO: add a multiplier for randomizer according to pawnType, then eliminate need for repeated checker
+                                health = pawnStats[pawnType].pawnSpawn[i].health + rand;
+                                // health = pawnStats[pawnType].pawnSpawn[i].health - 10;
+                                // TODO: need to create a way to increase/decrease health of "allies" by proximity (use adjacentSpaces function):
+                                if (health > -11 && health < 111) {
+
+                                    pawnStats[pawnType].pawnSpawn[i].health = health;
+                                    console.log(spaceToCheck.childNodes[0].id);
+                                    console.log("lastchild");
+                                    console.log(document.getElementById(homeSquare_ID).lastChild);
+                                    healthInfo(pawnType, pawnId);
+                                };
+                            };
+                        };
+                    } else if (currentSpace.contains("human-pawn")) {
+                        for (let i = 0; i < pawnStats[pawnType].pawnSpawn.length; i++) {
+
+                            homePawnLoc = pawnStats[pawnType].pawnSpawn[i].loc;
+
+                            console.log("homePawnLoc");
+                            console.log(homePawnLoc);
+
+                            console.log("homeSquare_ID");
+                            console.log(homeSquare_ID);
+
+                            if (homePawnLoc == homeSquare_ID) {
+                                console.log("they match!");
+                                let rand = Math.floor(Math.random() * 5);
+                                
+                                health = pawnStats[pawnType].pawnSpawn[i].health;
+                                // health = pawnStats[pawnType].pawnSpawn[i].health - 10;
+                                // TODO: need to create a way to increase/decrease health of "allies" by proximity (use adjacentSpaces function):
+                                if (health < 50) {
+                                    health = pawnStats[pawnType].pawnSpawn[i].health + rand;
+                                } else if (health > 50) {
+                                    health = pawnStats[pawnType].pawnSpawn[i].health - rand;
+                                } else {
+                                    health = pawnStats[pawnType].pawnSpawn[i].health;
+                                };
+                                
+                                if (health > -11 && health < 111) {
+
+                                    pawnStats[pawnType].pawnSpawn[i].health = health;
+                                    console.log(spaceToCheck.childNodes[0].id);
+                                    console.log("lastchild");
+                                    console.log(document.getElementById(homeSquare_ID).lastChild);
+                                    healthInfo(pawnType, pawnId);
+                                };
+                            };
+                        };
+                    };
 
                 } else if (spaceToCheck.childNodes[0].classList.contains("zombie-pawn")) {
 
